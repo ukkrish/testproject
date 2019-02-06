3dbc29995a68223138dfc5996d854149e48da53e
KrishG <contactkrishnag@gmail.com>
2019-02-06 22:25:24 +0000

    first commit to project

---
+++ src/test/java/testproj/Helloworld.java
@@ -0,0 +1,16 @@
+package testproj;
+
+import org.junit.Test;
+
+public class Helloworld {
+
+
+    @Test
+    public void sumofnums() {
+        int i = 5;
+        int j = 7;
+
+        System.out.println ("\n hello world, the total is: "+i+j);
+
+    }
+}
