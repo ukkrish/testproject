13e0efd9f2efd652181538c7fea512d90e7f7ca6
KrishG <contactkrishnag@gmail.com>
2019-02-06 22:35:11 +0000

    small commit to branch1

--- src/test/java/testproj/Helloworld.java
+++ src/test/java/testproj/Helloworld.java
@@ -10,8 +10,12 @@ public class Helloworld {
         int i = 5;
         int j = 7;
 
+        System.out.println ("\n ====================================== ");
         System.out.println ("\n hello world, the total is: "+i+j);
         System.out.println ("\n Test for GIT: "+(j-i));
+
+
+        System.out.println ("\n =============== B R A N C H  ======================= ");
 
     }
 }
